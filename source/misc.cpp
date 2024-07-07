f7e4b8782d15b581aa77f469595d5e4561319dd4
tttak <tttak@users.noreply.github.com>
2024-07-07 18:11:05 +0900

    エンジン表示名を修正

--- source/misc.cpp
+++ source/misc.cpp
@@ -190,7 +190,10 @@ const string engine_info() {
 			ENGINE_NAME
 #endif
 			<< ' '
-			<< EVAL_TYPE_NAME << ' '
+
+//			<< EVAL_TYPE_NAME << ' '
+			<< "NNUE HalfKP-KSDG_512x2-8-96" << ' '
+
 			<< ENGINE_VERSION << std::setfill('0')
 			<< (Is64Bit ? " 64" : " 32")
 			<< TARGET_CPU
